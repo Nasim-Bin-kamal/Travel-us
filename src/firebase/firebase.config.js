7f9fd9f1506de2fd5823f521902b9d93891d0f89
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2022-04-04 22:56:51 +0600

    firebase config done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,13 +1,4 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDmcXbiZl99iLZjVmbpUStq6Q160RabF04",
-    authDomain: "travel-us-30ded.firebaseapp.com",
-    projectId: "travel-us-30ded",
-    storageBucket: "travel-us-30ded.appspot.com",
-    messagingSenderId: "161306024226",
-    appId: "1:161306024226:web:7afc2c17ea4885ca22ad7b"
-};
-
-const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
